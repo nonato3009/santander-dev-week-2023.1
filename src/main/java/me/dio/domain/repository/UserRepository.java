f3e5470af70fb76eb8cf00b4a9141669623c2f49
nonato3009 <nonato3@hotmail.com>
2023-07-30 20:29:51 -0300

    Primeira publicação no Railway

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByAccountNumber(String accountNumber);
+
+    //boolean existsByAccountNumber();
+
+    //boolean existsByAccountNumber(String number);
 }
